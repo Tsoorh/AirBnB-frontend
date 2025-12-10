f3ba1e470846b6bfe6e90304e8959e31c5300eeb
Inbal Carmy <inbal950@gmail.com>
2025-12-10 17:50:55 +0200

    change x to svg

--- src/cmps/ImageGallery.jsx
+++ src/cmps/ImageGallery.jsx
@@ -69,7 +69,10 @@ export function ImageGallery({ images, alt, stayId }) {
           className="lightbox-close-button"
           onClick={handleCloseLightbox}
         >
-          X Close
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" aria-hidden="true" role="presentation" focusable="false" style={{ display: 'block', fill: 'none', height: '16px', width: '16px', stroke: 'currentcolor', strokeWidth: 3, overflow: 'visible' }}>
+            <path d="m6 6 20 20M26 6 6 26"></path>
+          </svg>
+           Close
         </button>
 
         
